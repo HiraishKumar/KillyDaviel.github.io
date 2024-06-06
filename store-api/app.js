59143d6544035a6c0ea08e69e8b8670fbdc7f552
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-06-06 23:58:43 +0530

    tets

--- store-api/app.js
+++ store-api/app.js
@@ -8,11 +8,28 @@ const errorMiddleware = require('./middleware/error-handler')
 
 // errorHandlerMiddleware
 app.use(express.json())
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
 
 // rootes
 
 app.get('/' , (res,req)=> {
-    res.send()
+    res.send('<h1>Store Api</h1><a  href="/api/v1/products" >Products route</a>')
 })
+
+const port = process.env.PORT || 3000
+
+const start = async () =>{
+    try {
+        // connect to DB
+        app.listen(port, (req,res)=>{
+            console.log(`Server is listening on port ${port}...`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
 
 console.log("This is a test")

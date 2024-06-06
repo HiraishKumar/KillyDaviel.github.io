148bf7270661a229e2a7ada0eb450923f44e59b2
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-06-06 19:40:46 +0530

    test

--- store-api/app.js
+++ store-api/app.js
@@ -6,4 +6,13 @@ const app = express();
 const notFoundMiddleware = require('./middleware/not-found')
 const errorMiddleware = require('./middleware/error-handler')
 
+// errorHandlerMiddleware
+app.use(express.json())
+
+// rootes
+
+app.get('/' , (res,req)=> {
+    res.send()
+})
+
 console.log("This is a test")

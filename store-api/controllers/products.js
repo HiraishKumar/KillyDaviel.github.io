62ed7d03d417c1e3239975b6812df2def8b97658
Killy <87876086+KillyDaviel@users.noreply.github.com>
2024-06-11 16:09:06 +0530

    added serache parameters

--- store-api/controllers/products.js
+++ store-api/controllers/products.js
@@ -2,25 +2,42 @@ const Product = require('../models/product')
 
 
 const getAllProductStatic = async (req,res)=>{
-    const products = await Product.find({
-        name: "vase table"
-    })
+    const search = "ab"
+    const products = await Product.find({}).select('name price')
     res.status(200).json({
         products , nbHits : products.length  
     })
 }
 
 const getAllProducts = async (req,res)=>{
-    const query = req.query
-    const products = await Product.find(
-        query
-    )
-    console.log(query)
+    const {featured,name,sort,fields} = req.query
+    const queryObject = {}
+    
+    if (featured){
+        queryObject.featured = featured === 'true' ? true : false
+    }
+    if (name){
+        queryObject.name = {$regex : name, $options:'i'}
+    }
+    // console.log(queryObject)
+    let result = Product.find(queryObject)
+    if (sort) {
+        const sortList = sort.split(',').join(' ')
+        result = result.sort(sortList)
+    }
+    else{
+        result.sort('createdAt')
+    }
 
+    if (fields) {
+        const fieldList = fields.split(',').join(' ')
+        result.select(fieldList)
+    } 
+
+    const product = await result
     res.status(200).json({
-        products , nbHits : products.length  
+        product , nbHits : product.length  
     })
-
 }
 
 module.exports = {
